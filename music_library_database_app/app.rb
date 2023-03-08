1bd58bf74aa1593aa95c0520fbb485eda8c99683
lizaebadi <liza-1@hotmail.co.uk>
2023-03-08 13:19:02 +0000

    Test driving challenge

--- music_library_database_app/app.rb
+++ music_library_database_app/app.rb
@@ -13,4 +13,47 @@ class Application < Sinatra::Base
     also_reload 'lib/album_repository'
     also_reload 'lib/artist_repository'
   end
+
+  get '/albums' do 
+    arr_album = []
+    repo = AlbumRepository.new 
+    albums = repo.all
+
+    albums.each do |album|
+      arr_album << album.title
+    end 
+    return arr_album.join(", ")
+  end 
+
+  post '/albums' do 
+    repo = AlbumRepository.new 
+
+    album = Album.new
+
+    album.title = params[:title]
+    album.release_year = params[:release_year]
+    album.artist_id = params[:artist_id]
+
+    repo.create(album)
+  end
+
+  get '/artists' do 
+    arr_artists = []
+    repo = ArtistRepository.new 
+    artists = repo.all 
+
+    artists.each do |artist|
+      arr_artists << artist.name 
+    end
+    return arr_artists.join(", ")
+  end 
+
+  post '/artists' do 
+    repo = ArtistRepository.new
+    new_artist = Artist.new
+    new_artist.name = params[:name]
+    new_artist.genre = params[:genre]
+
+    repo.create(new_artist)
+  end 
 end
--- music_library_database_app/spec/integration/application_spec.rb
+++ music_library_database_app/spec/integration/application_spec.rb
@@ -2,7 +2,23 @@ require "spec_helper"
 require "rack/test"
 require_relative '../../app'
 
+def reset_artists_table
+  seed_sql = File.read('spec/seeds/artists_seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+  connection.exec(seed_sql)
+end
+
+def reset_albums_table
+  seed_sql = File.read('spec/seeds/albums_seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+  connection.exec(seed_sql)
+end
+
 describe Application do
+  before(:each) do 
+    reset_albums_table
+    reset_artists_table
+  end
   # This is so we can use rack-test helper methods.
   include Rack::Test::Methods
 
@@ -10,4 +26,45 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context "GETS /albums" do
+    it 'returns 200 OK and all albums' do 
+      response = get('/albums')
+      expected_response = 'Doolittle, Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    end
+  end
+
+  context "POST /albums" do
+    it 'returns 200 OK' do
+      # When creating a new album.
+      response = post('/albums', title: 'Voyage', release_year: '2022', artist_id: '2')
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('')
+
+      result = get('/albums')
+      expect(result.body).to include("Voyage")
+    end
+  end 
+  
+  context "GET /artists" do 
+    it "returns response 200 ok and a list of artists" do 
+      response = get('/artists')
+      expected_response = "Pixies, ABBA, Taylor Swift, Nina Simone"
+      expect(response.body).to eq(expected_response)
+      expect(response.status).to eq(200)
+    end
+  end
+
+  context "POST /artists" do 
+    it "returns 200 ok and creates a new artist" do
+      response = post('/artists', name: "Wild nothing", genre: "Indie")
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('')
+
+      result = get('/artists')
+      expect(result.body).to include("Wild nothing")
+    end 
+  end 
 end
