a3e60455147eb3e3ab79f1d316b5f5030f4ab900
lizaebadi <liza-1@hotmail.co.uk>
2023-03-10 12:41:59 +0000

    using forms

--- music_library_database_app/app.rb
+++ music_library_database_app/app.rb
@@ -13,44 +13,15 @@ class Application < Sinatra::Base
     also_reload 'lib/album_repository'
     also_reload 'lib/artist_repository'
   end
-
+  
   get '/albums' do 
     repo = AlbumRepository.new 
     @albums = repo.all
     return erb(:albums)
   end 
 
-  post '/albums' do 
-    repo = AlbumRepository.new 
-
-    album = Album.new
-
-    album.title = params[:title]
-    album.release_year = params[:release_year]
-    album.artist_id = params[:artist_id]
-
-    repo.create(album)
-  end
-
-  get '/artists' do 
-    repo = ArtistRepository.new 
-    @artists = repo.all 
-    return erb(:artists)
-  end 
-
-  get '/artists/:id' do 
-    artist_repo = ArtistRepository.new 
-    @artist = artist_repo.find(params[:id])
-    return erb(:artist)
-  end 
-
-  post '/artists' do 
-    repo = ArtistRepository.new
-    new_artist = Artist.new
-    new_artist.name = params[:name]
-    new_artist.genre = params[:genre]
-
-    repo.create(new_artist)
+  get '/albums/new' do 
+    return erb(:new_album)
   end 
 
   get '/albums/:id' do 
@@ -61,6 +32,65 @@ class Application < Sinatra::Base
     @artist = artist_repo.find(@album.artist_id)
 
     return erb(:album)
+  end 
+
+  post '/albums' do 
+    def invalid_request_parameters? 
+      return true if params[:title] == nil || params[:release_year] == nil || params[:artist_id] == nil
+      return true if params[:title] == "" || params[:release_year] == "" || params[:artist_id] == ""
+      return false
+    end 
+
+    if invalid_request_parameters?
+      status 400
+      return ''
+    end 
+
+    repo = AlbumRepository.new 
+    album = Album.new
+    album.title = params[:title]
+    album.release_year = params[:release_year]
+    album.artist_id = params[:artist_id]
+    repo.create(album)
+  end
+
+
+
+# ARTISTS
+
+  get '/artists' do 
+    repo = ArtistRepository.new 
+    @artists = repo.all 
+    return erb(:artists)
+  end 
+
+  get '/artists/new' do 
+    return erb(:new_artist)
+  end 
+
+  get '/artists/:id' do 
+    artist_repo = ArtistRepository.new 
+    @artist = artist_repo.find(params[:id])
+    return erb(:artist)
+  end 
+
+  post '/artists' do 
+    def invalid_request_parameters? 
+      return true if params[:name] == nil || params[:genre] == nil 
+      return true if params[:name] == "" || params[:genre] == "" 
+      return false
+    end 
+
+    if invalid_request_parameters?
+      status 400
+      return ''
+    end 
+
+    repo = ArtistRepository.new
+    new_artist = Artist.new
+    new_artist.name = params[:name]
+    new_artist.genre = params[:genre]
+    repo.create(new_artist)
   end 
 
 end
--- music_library_database_app/spec/integration/application_spec.rb
+++ music_library_database_app/spec/integration/application_spec.rb
@@ -29,7 +29,6 @@ describe Application do
   context "GETS /albums" do
     it 'returns 200 OK and all albums' do 
       response = get('/albums')
-    #  expected_response = 'Doolittle, Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
 
       expect(response.status).to eq(200)
       expect(response.body).to include('<a href="/albums/1">Doolittle</a>')
@@ -37,10 +36,35 @@ describe Application do
     end
   end
 
+  context "GET /albums/new" do 
+    it "returns a new album form" do 
+      response = get('/albums/new')
+      expect(response.status).to eq(200)
+      expect(response.body).to include('<form action="/albums" method="POST">')
+      expect(response.body).to include('<input type="text" name="title">')
+      expect(response.body).to include('<input type="text" name="release_year">')
+      expect(response.body).to include('<input type="text" name="artist_id">')
+    end 
+  end 
+
+  context "GET /albums/:id" do 
+    it "return the first album" do 
+      response = get('/albums/1')
+      expect(response.body).to include("Doolittle")
+      expect(response.body).to include("Pixies")
+      expect(response.body).to include("1989")
+      expect(response.status).to eq(200)
+    end
+  end 
+
   context "POST /albums" do
-    it 'returns 200 OK' do
-      # When creating a new album.
-      response = post('/albums', title: 'Voyage', release_year: '2022', artist_id: '2')
+    it 'returns 200 OK when creating a new album' do
+      response = post(
+        '/albums', 
+        title: 'Voyage', 
+        release_year: '2022', 
+        artist_id: '2'
+      )
       expect(response.status).to eq(200)
       expect(response.body).to eq('')
 
@@ -49,6 +73,10 @@ describe Application do
     end
   end 
   
+
+# ARTISTS
+
+
   context "GET /artists" do 
     it "returns response 200 ok and a list of artists" do 
       response = get('/artists')
@@ -57,6 +85,16 @@ describe Application do
 
       expect(response.status).to eq(200)
     end
+  end
+
+  context "GET /artists/new" do 
+    it "returns a form to add a new artist" do 
+      response = get('/artists/new')
+      expect(response.status).to eq(200)
+      expect(response.body).to include('<form action="/artists" method="POST">')
+      expect(response.body).to include('<input type="text" name="name">')
+      expect(response.body).to include('<input type="text" name="genre">')
+    end 
   end
 
   context "GET /artists/:id" do 
@@ -70,7 +108,11 @@ describe Application do
 
   context "POST /artists" do 
     it "returns 200 ok and creates a new artist" do
-      response = post('/artists', name: "Wild nothing", genre: "Indie")
+      response = post(
+        '/artists', 
+        name: "Wild nothing", 
+        genre: "Indie"
+      )
       expect(response.status).to eq(200)
       expect(response.body).to eq('')
 
@@ -79,13 +121,4 @@ describe Application do
     end 
   end 
 
-  context "GET /albums/:id" do 
-    it "return the first album" do 
-      response = get('/albums/1')
-      expect(response.body).to include("Doolittle")
-      expect(response.body).to include("Pixies")
-      expect(response.body).to include("1989")
-      expect(response.status).to eq(200)
-    end
-  end 
 end
